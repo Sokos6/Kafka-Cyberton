0a4b176f701c89215475d172535e883c5bb00e47
Sokos6 <will.sokolowski@gmail.com>
2023-02-17 15:45:15 -0600

    quick logger init

--- kafka-core/src/main/java/io/cyberton/core/kafka/ProducerCore.java
+++ kafka-core/src/main/java/io/cyberton/core/kafka/ProducerCore.java
@@ -1,7 +1,13 @@
 package io.cyberton.core.kafka;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class ProducerCore {
+
+    private static final Logger log = LoggerFactory.getLogger(ProducerCore.class.getSimpleName());
+
     public static void main(String[] args) {
-        System.out.println("Cyberton Core Init");
+       log.info("Cyberton Core Init");
     }
 }
