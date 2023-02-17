05e936aa199290628fee72c8ac36a92b2163fe4a
Sokos6 <will.sokolowski@gmail.com>
2023-02-17 15:52:16 -0600

    producer properties

--- kafka-core/src/main/java/io/cyberton/core/kafka/ProducerCore.java
+++ kafka-core/src/main/java/io/cyberton/core/kafka/ProducerCore.java
@@ -1,7 +1,12 @@
 package io.cyberton.core.kafka;
 
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
 
 public class ProducerCore {
 
@@ -9,5 +14,11 @@ public class ProducerCore {
 
     public static void main(String[] args) {
        log.info("Cyberton Core Init");
+
+       // create Producer Properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
     }
 }
