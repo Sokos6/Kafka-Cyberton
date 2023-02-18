f20ec2ad322118a892142e5f93e1b39408a24e6d
Sokos6 <will.sokolowski@gmail.com>
2023-02-18 12:03:24 -0600

    producer finished and tested with consumer

--- kafka-core/src/main/java/io/cybertron/core/kafka/ProducerCore.java
+++ kafka-core/src/main/java/io/cybertron/core/kafka/ProducerCore.java
@@ -1,7 +1,8 @@
-package io.cyberton.core.kafka;
+package io.cybertron.core.kafka;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,12 +14,29 @@ public class ProducerCore {
     private static final Logger log = LoggerFactory.getLogger(ProducerCore.class.getSimpleName());
 
     public static void main(String[] args) {
-       log.info("Cyberton Core Init");
+       log.info("Cybertron Core Init");
 
        // create Producer Properties
         Properties properties = new Properties();
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // create the Producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        //create a producer record
+        ProducerRecord<String, String> producerRecord =
+                new ProducerRecord<>("cybertron_java", "Cybertron Connect");
+
+        // sent the data - async
+        producer.send(producerRecord);
+
+        // flush data - synchronous
+        producer.flush();
+
+        // flush and close producer
+        producer.close();
+
     }
 }
