36afc8b865fadce1bb446ed138cdef3cb738ead7
Sokos6 <will.sokolowski@gmail.com>
2023-02-19 12:31:11 -0600

    cooperative group config

---
+++ kafka-core/src/main/java/io/cybertron/core/kafka/ConsumerCoreCooperative.java
@@ -0,0 +1,54 @@
+package io.cybertron.core.kafka;
+
+
+import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+public class ConsumerCoreCooperative {
+
+    private static final Logger log = LoggerFactory.getLogger(ConsumerCoreCooperative.class.getSimpleName());
+
+    public static void main(String[] args) {
+       log.info("Cybertron Consumer Init");
+
+       String bootstrapServers = "127.0.0.1:9092";
+       String groupId = "second-app";
+       String topic = "cybertron_java";
+
+       // create Producer Properties
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        properties.setProperty(ConsumerConfig.PARTITION_ASSIGNMENT_STRATEGY_CONFIG, CooperativeStickyAssignor.class.getName());
+
+        // create consumer configs
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+
+        // subscribe consumer to our topic(s)
+        consumer.subscribe(Arrays.asList(topic));
+
+        // poll for new data
+        while(true) {
+            ConsumerRecords<String, String> records =
+                    consumer.poll(Duration.ofMillis(100));
+
+            for (ConsumerRecord<String, String> record : records) {
+                log.info("Key: " + record.key() + ", Value: " + record.value());
+                log.info("Partition: " + record.key() + ", Offset: " + record.offset());
+            }
+        }
+
+
+
+
+    }
+}
