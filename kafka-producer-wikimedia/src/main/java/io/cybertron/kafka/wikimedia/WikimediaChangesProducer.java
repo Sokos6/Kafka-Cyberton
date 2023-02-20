bd66a50dd05dea1884c1a66b9dac0087b9595378
Sokos6 <will.sokolowski@gmail.com>
2023-02-20 11:55:04 -0600

    wiki producer class

---
+++ kafka-producer-wikimedia/src/main/java/io/cybertron/kafka/wikimedia/WikimediaChangesProducer.java
@@ -0,0 +1,38 @@
+package io.cybertron.kafka.wikimedia;
+
+import com.launchdarkly.eventsource.EventSource;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.beans.EventHandler;
+import java.net.URI;
+import java.util.Properties;
+
+public class WikimediaChangesProducer {
+
+    public static void main(String[] args) {
+
+        String boostrapServers = "127.0.0.1:9092";
+
+        // create producer properties
+
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, boostrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // create the Producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        String topic = "wikimedia.recentchange";
+
+        EventHandler eventHandler = TODO;
+        String url = "https://stream.wikimedia.org/v2/stream/recenchange";
+        EventSource.Builder builder = new EventSource.Builder(eventHandler, URI.create(url));
+        EventSource eventSource = builder.build();
+
+        // start the producer in another thread
+        eventSource.start();
+    }
+}
