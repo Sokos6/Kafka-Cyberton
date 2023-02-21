64902e936888b63637c2043d70eecbacf586c70a
Sokos6 <will.sokolowski@gmail.com>
2023-02-20 18:18:14 -0600

    producer change

--- kafka-producer-wikimedia/src/main/java/io/cybertron/kafka/wikimedia/WikimediaChangesProducer.java
+++ kafka-producer-wikimedia/src/main/java/io/cybertron/kafka/wikimedia/WikimediaChangesProducer.java
@@ -1,17 +1,19 @@
 package io.cybertron.kafka.wikimedia;
 
+import com.launchdarkly.eventsource.EventHandler;
 import com.launchdarkly.eventsource.EventSource;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 
-import java.beans.EventHandler;
 import java.net.URI;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
 
 public class WikimediaChangesProducer {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         String boostrapServers = "127.0.0.1:9092";
 
@@ -27,12 +29,17 @@ public class WikimediaChangesProducer {
 
         String topic = "wikimedia.recentchange";
 
-        EventHandler eventHandler = TODO;
-        String url = "https://stream.wikimedia.org/v2/stream/recenchange";
+        EventHandler eventHandler = new WikimediaChangeHandler(producer, topic);
+        String url = "https://stream.wikimedia.org/v2/stream/recentchange";
         EventSource.Builder builder = new EventSource.Builder(eventHandler, URI.create(url));
         EventSource eventSource = builder.build();
 
+
+
         // start the producer in another thread
         eventSource.start();
+
+        // we produce for 10 minutes and block the program until then
+        TimeUnit.MINUTES.sleep(10);
     }
 }
